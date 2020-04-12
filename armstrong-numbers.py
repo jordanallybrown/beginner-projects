c4172cf763bb0f717f2f695477e5c71fff89fad1
jordanallybrown <jordanallybrown@gmail.com>
2020-04-11 22:53:48 -0400

    Enhance armstrong numbers to be able to compute with any number of places in n, more than 0-999

--- armstrong-numbers.py
+++ armstrong-numbers.py
@@ -3,28 +3,62 @@ Author: Jordan (jordanallybrown)
 Problem: An Armstrong number is a number such that the sum of its digits raised to the third power is equal to the
 number itself.
 
+Algorithm: Start with n as our dividend, then use the remainder of n by dividing by it's base (i.e. base 10) to
+isolate each digit. Multiply the remainder by 3 and add it to our total sum (according to Armstrong formula).
+Next divide by 10 to move to the next 10's place in the number, and repeat until our dividend has nothing more to
+divide it by (i.e. 0) because we've checked all places
+
 Lesson: make use of integer division in python and using the powers of 10 to isolate digits (in base 10)
 '''
 
+def getPower1(n):
+	'''
+
+	Return the number of places in n
+
+	:param n: positive integer
+	:return: int, number of places in the integer, n
+	'''
+	if n == 0:
+		return 1
+	else:
+		powerCounter = 0
+		while n > 0:
+			n = n//10
+			powerCounter += 1
+		return powerCounter
+
+def getPower2(n):
+	'''
+
+	Return the number of places in n (simulate a do-while in python)
+
+	:param n: positive integer
+	:return: int, number of places in the integer, n
+	'''
+	powerCounter = 0
+	while True:
+		n = n//10
+		powerCounter += 1
+		if n <= 0:
+			break
+	return powerCounter
 
 def isArmStrongNum(n):
 	'''
 	Determines whether integer n is an Armstrong number
-
-	Algorithm: Start with n as our dividend, then use the remainder of n by dividing by it's base (i.e. base 10) to
-	isolate each digit. Multiply the remainder by 3 and add it to our total sum (according to Armstrong formula).
-	Next divide by 10 to move to the next 10's place in the number, and repeat until our dividend has nothing more to
-	divide it by (i.e. 0) because we've checked all places
 	:param n: a positive integer
 	:return: True if n is an Armstrong number, False otherwise
 	'''
 	dividend = n
 	totalSum = remainder = 0
+	power = getPower1(n)
 	while dividend > 0:
 		remainder = dividend % 10
-		totalSum += remainder**3
+		totalSum += remainder**power
 		dividend = dividend // 10
 	return (totalSum == n)
+
 
 def main():
 
@@ -33,5 +67,6 @@ def main():
 	print(isArmStrongNum(1)) # True
 	print(isArmStrongNum(137)) # False
 	print(isArmStrongNum(371)) # True
+	print(isArmStrongNum(1634)) # True
 
 main()
