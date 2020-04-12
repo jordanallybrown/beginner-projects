23966846e12a7d8968bb5a26920ec2906f9eff4b
jordanallybrown <jordanallybrown@gmail.com>
2020-04-11 22:05:29 -0400

    Edit Armstrong comments

--- armstrong-numbers.py
+++ armstrong-numbers.py
@@ -11,11 +11,10 @@ def isArmStrongNum(n):
 	'''
 	Determines whether integer n is an Armstrong number
 
-	Algorithm: Use the remainder of n by dividing by it's base (i.e. base 10) to isolate each digit. Multiply the
-	remainder by 3 and add it to our total sum (according to Armstrong formula). Next divide by 10 to move to the next
-	10's place in the number, and repeat until our dividend has nothing more to divide it by (i.e. 0)
-	because we've checked all places
-
+	Algorithm: Start with n as our dividend, then use the remainder of n by dividing by it's base (i.e. base 10) to
+	isolate each digit. Multiply the remainder by 3 and add it to our total sum (according to Armstrong formula).
+	Next divide by 10 to move to the next 10's place in the number, and repeat until our dividend has nothing more to
+	divide it by (i.e. 0) because we've checked all places
 	:param n: a positive integer
 	:return: True if n is an Armstrong number, False otherwise
 	'''
